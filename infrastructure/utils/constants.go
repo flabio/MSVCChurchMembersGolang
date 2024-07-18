61c1ba1916312501b54631086d56722ac3ad00eb
Flabio Hinestroza Sanclemente <flabiohs@gmail.com>
2024-07-18 18:38:15 -0500

    MSVC church miembers

---
+++ infrastructure/utils/constants.go
@@ -0,0 +1,21 @@
+package utils
+
+const (
+	ID      = "id"
+	MESSAGE = "message"
+	STATUS  = "status"
+	DATA    = "data"
+
+	SUCCESS = "success"
+	FAIL    = "fail"
+	CREATED = "was successfully created"
+	UPDATED = "was updated successfully"
+	REMOVED = "was successfully removed"
+)
+
+const (
+	ID_NO_EXIST                  = "The id not exists"
+	NAME_ALREADY_EXIST           = "name already exists"
+	EMAIL_ALREADY_EXIST          = "email already exists"
+	IDENTIFICATION_ALREADY_EXIST = "identification already exists"
+)
